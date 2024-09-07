f6c453146ecc47232bd36bc9e635b1cc7ce15e97
mayowa <matthewoluwatotin2020@gmail.com>
2024-09-08 07:02:50 +1245

    add new file

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const app = express();
 app.use(express.static('public'));
 
 // Connect to MongoDB
-//mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  //  .then(() => console.log('Connected to MongoDB'))
-    //.catch(err => console.error('Could not connect to MongoDB...', err));
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
 
 // Set up EJS
 app.set('view engine', 'ejs');
